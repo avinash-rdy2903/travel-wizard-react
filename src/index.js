a6c0b552a44bc7b74392ab3aa4a9920d118f8b96
avinash-rdy2903 <mettukuruavinashreddy@gmail.com>
2023-04-25 01:42:22 -0400

    added disable dev tools

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
 import { GlobalStateProvider } from './globalState/globalState';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import App from './App';
 
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 ReactDOM.render(
 	<React.StrictMode>
 		{/* <BrowserRouter> */}
